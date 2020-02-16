c01ab6f4848dc3472202abab2771f37a86e738c4
Cameron <cdart2@illinois.edu>
2020-02-16 07:26:02 +0000

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,20 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
+  # Add Google timeservers
   networking.timeServers = options.networking.timeServers.default ++ [ "time.google.com" ];
 
   # Set your time zone.
@@ -46,32 +33,25 @@
     arandr				# GUI for xrandr
     scrot 				# Screen Locker
     fish				# Default Shell
+    jetbrains.clion 			# C/C++/Crust IDE
+    htop                                # Don't be a peasant
   ];
+
+  # Apply xkb settings outside of X11
+  # https://unix.stackexchange.com/questions/377600/in-nixos-how-to-remap-caps-lock-to-control
+  i18n.consoleUseXkbConfig = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
   # List services that you want to enable:
 
+  # Use chrony to keep your clocks up to date with networking.timeServers
   services.chrony.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
   # Settings for desktop, display, and window managers.
@@ -81,6 +61,7 @@
       default = "none";
       xterm.enable = false;
     };
+    xkbOptions = "caps:swapescape";
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [ dmenu
@@ -110,16 +91,4 @@
 
   # You'll be free hackers.
   nixpkgs.config.allowUnfree = true;
-
-  # Anduril cache things...
-  # Remove me if you're going to commit this to dotfiles repo.
-  nix.binaryCaches =
-      [ "https://cache.nixos.org/"
-        "https://s3-us-west-2.amazonaws.com/anduril-nix-cache"
-      ];
-  nix.binaryCachePublicKeys =
-      [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "anduril-nix-cache:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
-      ];
-
 }
